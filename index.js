4d0b64d1195d81a6df72383393600976d77f9b80
erisk405 <133034175+erisk405@users.noreply.github.com>
2024-08-02 09:13:19 +0700

    deleteApiOnIndex.js

--- index.js
+++ index.js
@@ -25,62 +25,6 @@ app.use(
 
 app.use("/api", routes);
 
-// PUT----------------------------------------------------------------
-
-app.put('/usersImage/:id', upload.single('image'), async (req, res) => {
-  try {
-      // URL ของรูปที่ถูกอัพโหลด
-      const userId = req.params.id;
-      // บันทึก URL ลงฐานข้อมูลโดยใช้ Prisma
-      const user = await prisma.user.findUnique({
-        where: {
-            id: userId
-          }
-      });
-      if (!user) {
-        return res.status(404).json({ error: 'User not found!' });
-      }
-      if (user.image_id) {
-        const oldImage = await prisma.image.findUnique({
-          where: { id: user.image_id },
-        });
-  
-        if (oldImage) {
-          console.log(`Deleting old image with public_id: ${oldImage.public_id}`);
-          // Delete the old image
-          await deleteImage(oldImage.public_id);
-  
-          // ลบ old image จาก database
-          await prisma.image.delete({
-            where: { id: oldImage.id },
-          });
-        }
-      }
-
-
-      // บันทึก Image ลงในตาราง Image
-      const newImage = await prisma.image.create({
-        data: {
-          url: req.file.path,
-          public_id: req.file.filename, // Assuming req.file.filename is the public_id
-        },
-      });
-  
-      // อัพเดต image_id ในตาราง User
-      const updatedUser = await prisma.user.update({
-        where: { id: userId },
-        data: { image_id: newImage.id },
-      });
-  
-      res.status(201).json(updatedUser);
-  } catch (error) {
-      console.error(error);
-      res.status(500).json({ error: 'Something went wrong!' ,message:error});
-  }
-});
-
-
-
 
 //start server
 const PORT = process.env.PORT || 8000;
